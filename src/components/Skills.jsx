dc0b4fdc960816a9e8388f1f10d790a95980ff89
RintaroKinashi <nagisa624174@gmail,com>
2022-03-02 13:07:42 +0900

    [update]refs #1_addition_of_axios

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+import axios from 'axios';
+
 export const Skills = () => {
+
+  // useEffect : レンダリング時に引数の関数を呼び出し
+  // axios : 非同期のhttp通信を簡単に行えるJSのライブラリ
+  useEffect(() => { axios
+    .get('https://api.github.com/users/RintaroKinashi/repos')
+    .then((response) => console.log(response)) }, []); // responseに上記の結果を格納する
+
   return (
     <div id="skills">
       <div className="container">
@@ -9,5 +19,5 @@ export const Skills = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
